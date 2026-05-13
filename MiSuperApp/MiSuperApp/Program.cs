65cce2142660249e72bc2754d3e462bd7995c262
ESTUDIANTE <ESTUDIANTE@DESKTOP-L55550A>
2026-05-12 20:13:04 -0500

    Hice el cambio

--- MiSuperApp/MiSuperApp/Program.cs
+++ MiSuperApp/MiSuperApp/Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
             Console.WriteLine("Otro cambio");
+            /*COementario*/
         }
     }
 }
